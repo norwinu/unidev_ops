2d51263a7bbe86015a2592c3d1b0d857c30ad8ab
norwinu <norwinu@unipharm.com>
2017-05-29 14:35:43 -0700

    1

---
+++ hw.py
@@ -0,0 +1,15 @@
+import ibm_db_dbi as dbi
+
+print('Test DB2 Connect.')
+
+def listRecords():
+    try:
+        db2conn = dbi.connect(dsn=None, database='*LOCAL', user=None, password=None)
+        db2curry = db2conn.cursor()
+        db2curry.execute("SELECT * FROM WEBPRDD.POSFTPUSRP")
+        result = db2curry.fetchall()
+
+    except Exception as e:
+        return 'Exception' + str(e)
+
+listRecords()
